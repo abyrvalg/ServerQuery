d0a02f7c7203d0ac8f5b8be1aef708305464c691
master <master@master>
2019-03-30 15:22:39 +0200

    added promises

--- index.js
+++ index.js
@@ -3,34 +3,44 @@ var globalConfig = {},
 	allResources = {};
 	
 function executeCall(query){
-	var obj = {};
+	var obj = {},
+		promise;
 	for(let i = 0; i < query.length; i++) {
+		let result,currentKey;
 		if(typeof query[i] == 'string') {
-			console.log(query[i]);
-			obj[query[i]] = allResources[query[i]]();
-			continue;
+			currentKey = query[i]
+			result = allResources[currentKey]();			;			
 		}
-		for(let key in query[i]){
-			try {
-				var params = query[i][key] && query[i][key].join('|').replace(/_([\w\.]+)/g, function(match, key){
-						let path = key.split('.'),
-							val = obj;
-						for(let i = 0; i < path.length; i++){
-							if(!val) {
-								break;
+		else {
+			for(let key in query[i]){
+				try {
+					var params = query[i][key] && query[i][key].join('|').replace(/_([\w\.]+)/g, function(match, key){
+							let path = key.split('.'),
+								val = obj;
+							for(let i = 0; i < path.length; i++){
+								if(!val) {
+									break;
+								}
+								val = val[path[i]];
 							}
-							val = val[path[i]];
-						}
-						return val || match;
-					}).split('|');
-				obj[key] = allResources[key].apply(allResources[key], params);
+							return val || match;
+						}).split('|');
+					result = allResources[key].apply(allResources[key], params);
+					currentKey = key;
+				}
+				catch(e){
+					console.log(e);
+				}
 			}
-			catch(e){
-				console.log(e);
-			}
-		}		
+		}
+		if(result instanceof Promise){
+			promise = result.then(function(r){obj[currentKey] = r; return obj});
+		}
+		else {
+			obj[currentKey] = result;
+		}
 	}
-	return obj;
+	return promise || Promise.resolve(obj);
 }
 module.exports = {
 	setConfig(config){
@@ -42,28 +52,36 @@ module.exports = {
 	setupRoute(route, app){
 		app.all(route, function(req, resp){
 			try{
-				var result = executeCall(JSON.parse(req.query.query));
-				resp.json(result);
+				var promise = executeCall(JSON.parse(req.query.query));
+				promise.then((result)=>{
+					resp.header("Content-Type", "application/json");
+					resp.json(result)
+				});
 			}
 			catch(e){
 				console.log(e);
 			}
 		});
 	},
-	call(query, cb, template){
-		var result = executeCall(query);
+	call(query, template){
+		var promise = executeCall(query);
 		if(!template) {
-			cb(result);
+			return promise;
 		}
 		var fs = require('fs');
-		fs.readFile('templates/'+template.replace(/\./g, '')+'.hbs', function(err, data){
-			if (!err) {
-				var source = data.toString();
-				template = HB.compile(source)
-				cb(template(result));
-			} else {
-				console.log(err);
-			}
-		});
+		return promise.then(function(result){
+			return new Promise(function(resolver){
+				fs.readFile('templates/'+template.replace(/\./g, '')+'.hbs', function(err, data){
+					if (!err) {
+						var source = data.toString();
+						template = HB.compile(source)
+						resolver(template(result));
+					} else {
+						console.log(err);
+					}
+				});
+			});			
+		})
+		
 	}
 }
