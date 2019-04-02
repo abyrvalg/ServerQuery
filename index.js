f16774dfaffad6ee49c5a3e13d59df3c10b23e34
Sergey Lozhechnikov <s.lozhechnikov@astoundcommerce.com>
2019-04-02 17:18:57 +0300

    Added possibility to add resource folder

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const HB = require('handlebars');
-var globalConfig = {},
+const path = require('path');
+var resourceFolder,
 	allResources = {};
-	
+function getResourceFunction(key){
+	return allResources[key] || (()=>{
+		var match = key.match(/((?:\w+)+)?\.(\w+)$/);
+		return require(path.dirname(require.main.filename) + '/'+resourceFolder+'/'+(match && match[1].replace(/\./g, '/') || '/index'))[match && match[2] || 'index']
+	})();
+}
+
+function getParams(key, obj){
+	return key && key.join('|').replace(/_([\w\.]+)/g, function(match, key){
+		let path = key.split('.'),
+			val = obj;
+		for(let i = 0; i < path.length; i++){
+			if(!val) {
+				break;
+			}
+			val = val[path[i]];
+		}
+		return val || match;
+	}).split('|');
+}
 function executeCall(query){
 	var obj = {},
 		promise;
@@ -9,25 +29,14 @@ function executeCall(query){
 		let result,currentKey;
 		if(typeof query[i] == 'string') {
 			currentKey = query[i]
-			result = promise ? promise.then((obj)=>allResources[currentKey]()) : allResources[currentKey]();			;			
+			result = promise ? promise.then((obj)=>getResourceFunction(currentKey)()) : getResourceFunction(currentKey)();			;			
 		}
 		else {
 			for(let key in query[i]){
 				try {					
 					result = promise ? promise.then((obj)=>{
-						var params = query[i][key] && query[i][key].join('|').replace(/_([\w\.]+)/g, function(match, key){
-							let path = key.split('.'),
-								val = obj;
-							for(let i = 0; i < path.length; i++){
-								if(!val) {
-									break;
-								}
-								val = val[path[i]];
-							}
-							return val || match;
-						}).split('|');
-						return allResources[key].apply(allResources[key], params)
-					}) : allResources[key].apply(allResources[key], params);
+						return getResourceFunction(key).apply(null, getParams(query[i][key], obj))
+					}) : getResourceFunction(key).apply(null, getParams(query[i][key], obj));
 					currentKey = key;
 				}
 				catch(e){
@@ -45,11 +54,11 @@ function executeCall(query){
 	return promise || Promise.resolve(obj);
 }
 module.exports = {
-	setConfig(config){
-		globalConfig = config;
-	},
 	addResources(resources){
 		Object.assign(allResources, resources);
+	},
+	setResourceFolder(folder){
+		resourceFolder = folder;
 	},
 	setupRoute(route, app){
 		app.all(route, function(req, resp){
