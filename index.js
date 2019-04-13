c31ec7743214b3b9699c94d6a37111d568087d1a
Sergey Lozhechnikov <s.lozhechnikov@astoundcommerce.com>
2019-04-13 21:58:49 +0300

    added "buffer" and dominant mode, added multiple resource folder

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const HB = require('handlebars');
 const PATH = require('path');
-var resourceFolder,
+var resourceFolders = [],
 	allResources = {};
 function getResourceFunction(key){
 	return allResources[key] || (()=>{
 		var match = key.match(/((?:[^_])+)??(?:\_(\w+))?$/);
-		return require(PATH.dirname(require.main.filename) + '/'+resourceFolder+'/'+(match && match[1].replace(/\./g, '/') || '/index'))[match && match[2] || 'index']
+		for(let i = 0; i < resourceFolders.length; i++){
+			let func;
+			try{
+				func = require(PATH.dirname(require.main.filename) + '/'+resourceFolders[i]+'/'+(match && match[1].replace(/\./g, '/') || '/index'))[match && match[2] || 'index'];
+			}
+			catch(e){
+				//console.log(e);
+			}
+			if(func) {
+				return func
+			}
+		}
 	})();
 }
 
 function getParams(key, obj){
 	return JSON.parse(key && JSON.stringify(key).replace(/"?_([\w\.]+)"?/g, function(match, key){
+		if(key == 'this') {
+			return JSON.stringify(obj);
+		}
 		let path = key.split('.'),
 			val = obj;
 		for(let i = 0; i < path.length; i++){
@@ -22,26 +36,35 @@ function getParams(key, obj){
 		if(typeof val == 'object') {
 			val = JSON.stringify(val);
 		}
-		return val || match;
+		return val;
 	}));
+}
+function parseKey(key){
+	var match = key.match(/^([\?\!]+)?(\w+)(?:\>(\w+))?/);
+	return {
+		gettterKey : match[2],
+		mode : {'?' : 'buffer', '!' : 'dominant'}[match[1]] || 'standard',
+		settterKey : match[3] || match[2]
+	}
 }
 function executeCall(query){
 	var obj = {},
+		buffer = {},
 		promise;
 	for(let i = 0; i < query.length; i++) {
 		let result,currentKey;
 		if(typeof query[i] == 'string') {
-			currentKey = query[i].split('>');
-			result = promise ? promise.then((obj)=>getResourceFunction(currentKey[0])()) : getResourceFunction(currentKey[0])();			;			
+			currentKey = parseKey(query[i]);
+			result = promise ? promise.then((obj)=>getResourceFunction(currentKey.gettterKey)()) : getResourceFunction(currentKey.gettterKey)();			;			
 		}
 		else {
 			for(let key in query[i]){
-				currentKey = key.split('>');
+				currentKey = parseKey(key);
 				try {					
 					result = promise ? promise.then((obj)=>{
 
-						return getResourceFunction(currentKey[0]).apply(null, getParams(query[i][key], obj));
-					}) : getResourceFunction(currentKey[0]).apply(null, getParams(query[i][key], obj));
+						return getResourceFunction(currentKey.gettterKey).apply(null, getParams(query[i][key], buffer));
+					}) : getResourceFunction(currentKey.gettterKey).apply(null, getParams(query[i][key], buffer));
 				}
 				catch(e){
 					console.log(e);
@@ -50,12 +73,25 @@ function executeCall(query){
 		}
 		if(result instanceof Promise){
 			promise = result.then(function(r){
-				obj[currentKey.length > 1 ? currentKey[1] : currentKey[0]] = r; 
+				if(currentKey.mode == "standard") {
+					obj[currentKey.settterKey] = r; 
+				}
+				else if(currentKey.mode == "dominant") {
+					obj = r;
+				}
+				buffer[currentKey.settterKey] = r;
+
 				return obj
 			});
 		}
 		else {
-			obj[currentKey.length > 1 ? currentKey[1] : currentKey[0]] = result;
+			if(currentKey.mode == "standard") {
+				obj[currentKey.settterKey] = r; 
+			}
+			else if(currentKey.mode == "dominant") {
+				obj = r;
+			}
+			buffer[currentKey.settterKey] = r;
 		}
 	}
 	return promise || Promise.resolve(obj);
@@ -64,16 +100,16 @@ module.exports = {
 	addResources(resources){
 		Object.assign(allResources, resources);
 	},
-	setResourceFolder(folder){
-		resourceFolder = folder;
+	addResourceFolder(folder){
+		resourceFolders.push(folder);
+		return this;
 	},
 	setupRoute(route, app){
 		app.all(route, function(req, resp){
 			try{
 				var promise = executeCall(JSON.parse(req.query.query));
 				promise.then((result)=>{
-					resp.header("Content-Type", "application/json");
-					resp.json(result)
+					resp.send(result)
 				});
 			}
 			catch(e){
@@ -82,23 +118,6 @@ module.exports = {
 		});
 	},
 	call(query, template){
-		var promise = executeCall(query);
-		if(!template) {
-			return promise;
-		}
-		var fs = require('fs');
-		return promise.then(function(result){
-			return new Promise(function(resolver){
-				fs.readFile('templates/'+template.replace(/\./g, '')+'.hbs', function(err, data){
-					if (!err) {
-						data = data.toString();
-						template = HB.compile(data);
-						resolver(template(result));
-					} else {
-						console.log(err);
-					}
-				});
-			});			
-		})		
+		return executeCall(query);
 	}
 }
